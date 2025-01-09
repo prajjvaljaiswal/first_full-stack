9ecc0339d4e1b2c2b7dc0abf05ff2140bdf40377
prajjvaljaiswal <prajjaiswal158@gmail.com>
2025-01-09 18:34:28 +0530

    - initialise the server at port 5000

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+app.use('/',(req, res)=>{
+    res.send("hello world")
+});
+
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("Server is Online!!")
+});

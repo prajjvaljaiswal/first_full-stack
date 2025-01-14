559c30b908ce965e3783c813af7d2ea0ab76d2b8
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-01-14 14:09:35 +0530

    - added token verifier and cookies

--- src/app.js
+++ src/app.js
@@ -1,45 +1,75 @@
 const express = require("express")
 const { connectDb } = require("./configs/database.js")
+const jwt = require("jsonwebtoken")
 const app = express()
 const User = require("./models/user.js")
+const cookieParser = require("cookie-parser")
 
+app.use(cookieParser())
 app.use(express.json())
 
 app.get("/signin", async (req, res) => {
     const email = req.body.email
     const password = req.body.password
-    if(!email || !password)
-        res.send("Enter email and password")
 
-    try { 
-        const user = await User.find({email: email})
+    // const {token} = req.cookies
+    // const decode = await jwt.verify(token,"prajjval2004")
+    // console.log(decode)
+    // console.log(req.cookies)
+
+    if (!email || !password)
+        res.send("Enter email and password")
+    try {
+        const user = await User.findOne({ email: email })
         console.log(user)
+        const token = jwt.sign({ _id: user._id }, "prajjval2004")
+        res.cookie("token", token)
         res.send(user)
-    } catch (err) { 
-        console.error("sign in failed: "+err)
+    } catch (err) {
+        console.error("sign in failed: " + err)
+    }
+});
+
+app.get('/profile', async (req, res) => {
+    try {
+        const { token } = req.cookies
+        if (!token)
+            throw new Error("Invlaid token")
+        const decode = await jwt.verify(token, "prajjval2004");
+        const { _id } = decode
+        const user = await User.findById(_id)
+        if (!user)
+            throw new Error("User not found")
+        res.send(user)
+    } catch (err) {
+        res.status(400).send("Error: " + err)
     }
 });
 
 app.post("/signup", async (req, res) => {
     const user = req.body
     const newUser = new User(user)
-    try { 
+
+    try {
+
         await newUser.save()
+        const token = jwt.sign({ _id: newUser._id }, "prajjval2004")
+        res.cookie("token", token)
         res.send("done")
-    } catch (err) { 
-        console.error("error while sign up: "+err)
+    } catch (err) {
+        console.error("error while sign up: " + err)
     }
 });
 
 app.patch("/user", async (req, res) => {
     // const name = req.body.firstname
     const id = req.body.id
-    try { 
+    try {
         // const user = await User.find({firstname: name})
-        await User.findByIdAndUpdate(id,req.body.update)
+        await User.findByIdAndUpdate(id, req.body.update)
         res.send("update sucessfull")
-    } catch (err) { 
-        console.error("update failed: "+err)
+    } catch (err) {
+        console.error("update failed: " + err)
     }
 });
 
